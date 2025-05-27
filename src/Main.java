5a82e2da76b75087fac81a03a0c79dbda0a29d84
checker888 <ort030627@gmail.com>
2025-05-27 21:12:12 +0900

    gcd、powerの修正

--- src/Main.java
+++ src/Main.java
@@ -67,21 +67,21 @@ public class Main {
     
     
     public static void dfsStack(Graph g,int start) {
-        Deque<Integer> dfsst = new ArrayDeque<>();//dfs用スタック
+        Deque<Integer> st = new ArrayDeque<>();//dfs用スタック
         if(g.visited[start] == 0) {
             g.visited[start] = 1;
-            dfsst.push(start);
+            st.push(start);
         }
-        while(!dfsst.isEmpty()) {
+        while(!st.isEmpty()) {
 
-            int pos = dfsst.pop();
+            int pos = st.pop();
             int size = g.adlist.get(pos).size();
             for(int i=0;i<size;i++) {
                 int to = g.adlist.get(pos).get(i);
                 if(g.visited[to] == 0) {
                     g.visited[to] = 1;
                     //ここに処理入れる
-                    dfsst.push(to);
+                    st.push(to);
                     
                 }
             }
@@ -136,6 +136,7 @@ public class Main {
     //ダイクストラ法
     
     public static void dijkstra(WeightedGraph g,int pos) {//最初の頂点posから各頂点までの距離を求める
+        g.resetCur();
         PriorityQueue<Pair<Long,Integer>> dijkpq = new PriorityQueue<Pair<Long,Integer>>();//pair(最小コスト候補、頂点)
         g.cur[pos] =0;
         dijkpq.add(new Pair<Long, Integer>(g.cur[pos], pos));
@@ -146,8 +147,8 @@ public class Main {
             
             g.visited[pos] = 1;
             for(int i = 0;i < g.pairadlist.get(pos).size();i++) {
-                int to = g.pairadlist.get(pos).get(i).getLeft();
-                long cost = g.pairadlist.get(pos).get(i).getRight();
+                int to = g.pairadlist.get(pos).get(i).to;
+                long cost = g.pairadlist.get(pos).get(i).cost;
                 if(g.cur[to] > g.cur[pos]+cost) {
                     g.cur[to] = g.cur[pos]+cost;
                     dijkpq.add(new Pair<Long, Integer>(g.cur[to], to));
@@ -167,11 +168,11 @@ public class Main {
         }
         //初期状態の設定(隣接する頂点のみのコスト)
         for(int from=0;from<=g.n;from++) {
-            ArrayList<Pair<Integer,Long>> a = g.pairadlist.get(from);
+            ArrayList<Edge> a = g.pairadlist.get(from);
             for(int j=0;j<a.size();j++) {
-                Pair<Integer,Long> p = a.get(j);
-                int to = p.getLeft();
-                dp[from][to] = p.getRight();
+                Edge p = a.get(j);
+                int to = p.to;
+                dp[from][to] = p.cost;
                 
             }
         }
@@ -192,6 +193,7 @@ public class Main {
     //最小全域木の構造ごと返す
     public static WeightedGraph kruskal(WeightedGraph g) {
         WeightedGraph result = new WeightedGraph(g.n);
+        UnionFind uf = new UnionFind(g.n+1);
         Collections.sort(g.edges, new Comparator<int[]>() {//コストの小さい順にソート
             @Override
             public int compare(int []edge1, int []edge2) {
@@ -203,9 +205,10 @@ public class Main {
             int u = edge[0];
             int v = edge[1];
             long cost = edge[2];
-            if(!result.uf.same(u, v)) {
+            if(!uf.same(u, v)) {
 //                result.connect(u, v, cost);//有向グラフのとき
                 result.connectMutual(u, v, cost);//無向グラフのとき (コスト総和の計算も関数内で行われてる
+                uf.unite(u, v);
             }
         }
         return result;
@@ -711,12 +714,12 @@ public class Main {
     
     //最大公約数求める
     static long gcd(long a, long b) {
-        while(a>=1 && b>=1) {
-            if(a >= b) a %= b;
-            else b %= a;
+        while (b != 0) {
+            long tmp = a % b;
+            a = b;
+            b = tmp;
         }
-        if(a != 0) return a;
-        else return b;
+        return a;
     }
     //最小公倍数求める
     static long lcm(long a, long b) {
@@ -724,15 +727,11 @@ public class Main {
     }
     //aのb乗求める
     static long power(long a, long b) {
-        long p = a;
         long ans = 1;
-        int lim = 30;
-        for(int i=0;i<lim;i++) {
-            int wari = (1 << i);
-            if((b/wari) % 2 == 1) {
-                ans= (ans * p);
-            }
-            p = (p*p);
+        while (b > 0) {
+            if ((b & 1) == 1) ans *= a; //ビットが立っているときだけ掛ける
+            a *= a;                     //底を二乗
+            b >>= 1;                    //指数を1bit右シフト
         }
         return ans;
     }
@@ -841,155 +840,141 @@ public class Main {
 
 //重みなしグラフ
 class Graph {
-    ArrayList<ArrayList<Integer>> adlist = new ArrayList<ArrayList<Integer>>();//隣接リスト
-    int n;
-    int visited[];
-    UnionFind uf;
-    //コンストラクタ
-    public Graph(int n) {
-        this.n = n;
-        for(int i=0; i<=n; i++){
-            ArrayList<Integer> ar = new ArrayList<Integer>();   //外側のList生成
-            this.adlist.add(ar);   
-            
-        }
-        this.visited = new int[n+1];
-        this.uf = new UnionFind(n+1);
-    }
-    
-    //単一方向（uからv）に頂点を接続
-    public void connect(int u, int v) {
-        this.adlist.get(u).add(v);
-        this.uf.unite(u, v);
-    }
-    //双方向に頂点を接続
-    public void connectMutual(int u, int v){
-        this.adlist.get(u).add(v);
-        this.adlist.get(v).add(u);
-        this.uf.unite(u, v);
-    }
-    //unionfindで2つの頂点が繋がっているか確認
-    public boolean isConnect(int u, int v) {return this.uf.same(u, v);}
-    
-    //探索済み（または最短距離）を示すvisitedのリセット
-    public void resetVisited() {this.visited = new int[n+1];}
+  ArrayList<ArrayList<Integer>> adlist = new ArrayList<ArrayList<Integer>>();//隣接リスト
+  int n;
+  int visited[];
+  //コンストラクタ
+  public Graph(int n) {
+      this.n = n;
+      for(int i = 0; i <= n; i++) adlist.add(new ArrayList<>());
+      this.visited = new int[n+1];
+  }
+  
+  //単一方向（uからv）に頂点を接続
+  public void connect(int u, int v) {
+      this.adlist.get(u).add(v);
+//      if (!adlist.get(u).contains(v)) adlist.get(u).add(v);
+  }
+  //双方向に頂点を接続
+  public void connectMutual(int u, int v){
+      this.adlist.get(u).add(v);
+      this.adlist.get(v).add(u);
+  }
+  //unionfindで2つの頂点が繋がっているか確認
+//  public boolean isConnect(int u, int v) {return this.uf.same(u, v);}
+  //探索済み（または最短距離）を示すvisitedのリセット
+  public void resetVisited(int value) { Arrays.fill(this.visited, value);}
 }
 
 //重みつきグラフ
 class WeightedGraph {
-    static long biglong = 2000000000000000000L;
-    ArrayList<ArrayList<Pair<Integer,Long>>> pairadlist;//pair(移動先の頂点、コスト)
-    
-    List<int[]> edges ;
-    int n;
-    int visited[];
-    long cur[];//ダイクストラ法で使う、頂点までのコストの総和の最小値
-    long costsum;//クラスカル法で使う、すべての辺のコストの総和
-    UnionFind uf;
-    //コンストラクタ
-    public WeightedGraph(int n) {
-        pairadlist = new ArrayList<ArrayList<Pair<Integer,Long>>>();
-        edges = new ArrayList<int[]>();
-        this.n = n;
-        for(int i=0; i<=n; i++){
-            ArrayList<Pair<Integer,Long>> ar = new ArrayList<Pair<Integer,Long>>();
-            this.pairadlist.add(ar);   
-            
-        }
-        this.visited = new int[n+1];
-        this.cur = new long[n+1];
-        this.uf = new UnionFind(n+1);
-        for(int i=0;i<=n;i++) cur[i] = biglong;
-    }
-    
-    //単一方向（uからv）に頂点を接続
-    public void connect(int u, int v, long cost) {
-        this.pairadlist.get(u).add(new Pair<Integer, Long>(v,cost));
-        
-        
-        this.uf.unite(u, v);
-        this.costsum += cost;
-        edges.add(new int[] { u, v ,(int)cost });
-    }
-    
-    //双方向に頂点を接続
-    public void connectMutual(int u, int v, long cost){
-        this.pairadlist.get(u).add(new Pair<Integer, Long>(v, cost));
-        this.pairadlist.get(v).add(new Pair<Integer, Long>(u, cost));
-        
-        
-        this.uf.unite(u, v);
-        this.costsum += cost;
-        edges.add(new int[] { u, v ,(int)cost });
-    }
-    
-    //unionfindで2つの頂点が繋がっているか確認
-    public boolean isConnect(int u, int v) {return this.uf.same(u, v);}
-    
-    //探索済み（または最短距離）を示すvisitedのリセット
-    public void resetVisited() {this.visited = new int[n+1];}
-    
-    public void resetCur() {for(int i=0;i<=n;i++) cur[i] = biglong;}
+  static long biglong = 2000000000000000000L;
+  ArrayList<ArrayList<Edge>> pairadlist;//pair(移動先の頂点to、コストcost)
+  
+  List<int[]> edges ;
+  int n;
+  int visited[];
+  long cur[];//ダイクストラ法で使う、頂点までのコストの総和の最小値
+  long costsum;//クラスカル法で使う、すべての辺のコストの総和
+  //コンストラクタ
+  public WeightedGraph(int n) {
+      pairadlist = new ArrayList<ArrayList<Edge>>();
+      edges = new ArrayList<int[]>();
+      this.n = n;
+      for(int i=0; i<=n; i++)this.pairadlist.add(new ArrayList<Edge>());
+      this.visited = new int[n+1];
+      this.cur = new long[n+1];
+  }
+  
+  //単一方向（uからv）に頂点を接続
+  public void connect(int u, int v, long cost) {
+      this.pairadlist.get(u).add(new Edge(v,cost));
+      this.costsum += cost;
+      edges.add(new int[] { u, v ,(int)cost });
+  }
+  
+  //双方向に頂点を接続
+  public void connectMutual(int u, int v, long cost){
+      this.pairadlist.get(u).add(new Edge(v, cost));
+      this.pairadlist.get(v).add(new Edge(u, cost));
+      this.costsum += cost;
+      edges.add(new int[] { u, v ,(int)cost });
+  }
+  //unionfindで2つの頂点が繋がっているか確認
+//  public boolean isConnect(int u, int v) {return this.uf.same(u, v);}
+  //探索済み（または最短距離）を示すvisitedのリセット
+  public void resetVisited(int value) { Arrays.fill(this.visited, value);}
+  
+  public void resetCur() {Arrays.fill(cur, biglong);}
 }
 
 
 class UnionFind {
-    int[] parent;
-    int[] rank;
-    //コンストラクタ
-    public UnionFind(int n) {
-        this.parent = new int[n];
-        this.rank = new int[n];
-        // 最初はすべてが根(or根はない)
-        for (int i = 0; i < n; i++) {
-            parent[i] = i;
-//            parent[i] = -1; //根はない
-            rank[i] = 0;
-        }
-    }
-//     要素の根を経路圧縮して返す
-    public int root(int x) {
-        if(x != parent[x]) {
-            parent[x] = root(parent[x]);
-        }
-        return parent[x];
-    }
-    //２つの要素が同じ集合に属するかどうかを返す
-    public boolean same(int x, int y) {
-        return root(x) == root(y);
-    }
-    //xの属する集合とyの属する集合をつなげる
-    public void unite(int x, int y) {
-        x = root(x);
-        y = root(y);
+  int[] parent;
+  int[] rank;
+  //コンストラクタ
+  public UnionFind(int n) {
+      this.parent = new int[n];
+      this.rank = new int[n];
+      // 最初はすべてが根(or根はない)
+      for (int i = 0; i < n; i++) {
+          parent[i] = i;
+//          parent[i] = -1; //根はない
+          rank[i] = 0;
+      }
+  }
+//   要素の根を経路圧縮して返す
+  public int root(int x) {
+      if(x != parent[x]) {
+          parent[x] = root(parent[x]);
+      }
+      return parent[x];
+  }
+  //２つの要素が同じ集合に属するかどうかを返す
+  public boolean same(int x, int y) {
+      return root(x) == root(y);
+  }
+  //xの属する集合とyの属する集合をつなげる
+  public void unite(int x, int y) {
+      x = root(x);
+      y = root(y);
 
-        if (x == y)  return;// 属する集合が同じな場合、何もしない
+      if (x == y)  return;// 属する集合が同じな場合、何もしない
 
-        if (rank[x] > rank[y]) {
-            parent[y] = x;
-        } else {
-            parent[x] = y;
-            if(rank[x] == rank[y]) rank[y]++;
-        } 
-    }
+      if (rank[x] > rank[y]) {
+          parent[y] = x;
+      } else {
+          parent[x] = y;
+          if(rank[x] == rank[y]) rank[y]++;
+      } 
+  }
+}
+
+class Edge {
+  int to;
+  long cost;
+  Edge(int to, long cost) {
+      this.to = to;
+      this.cost = cost;
+  }
 }
 
 class Pair<S extends Comparable<S>, T extends Comparable<T>> implements Comparable<Pair<S,T>>{//ソートできるようにComparable使う
-    S left;
-    T right;
+  S left;
+  T right;
 
-    public Pair(S s, T t){
-        left = s;
-        right = t;
-    }
-    //getメソッドは、必ず代入してから使う　（if文などで比較したとき、intをInteger型で比較して失敗したりすることに気づかない）
-    public S getLeft(){return left;}
-    public T getRight(){return right;}
+  public Pair(S s, T t){
+      left = s;
+      right = t;
+  }
+  //getメソッドは、必ず代入してから使う　（if文などで比較したとき、intをInteger型で比較して失敗したりすることに気づかない）
+  public S getLeft(){return left;}
+  public T getRight(){return right;}
 
-    public int compareTo(Pair<S,T> another){
-        java.util.Comparator<Pair<S,T>> comp1 = java.util.Comparator.comparing(Pair::getLeft);
-        java.util.Comparator<Pair<S,T>> comp2 = comp1.thenComparing(Pair::getRight);
-        return comp2.compare(this, another);
-    }
+  public int compareTo(Pair<S,T> another){
+      java.util.Comparator<Pair<S,T>> comp1 = java.util.Comparator.comparing(Pair::getLeft);
+      java.util.Comparator<Pair<S,T>> comp2 = comp1.thenComparing(Pair::getRight);
+      return comp2.compare(this, another);
+  }
 
 }

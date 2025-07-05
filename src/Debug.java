974d15fe15c3fa47a139e0523360fb8517acb4e3
checker888 <ort030627@gmail.com>
2025-07-06 04:55:03 +0900

    DebugからのMain実行を関数化

--- src/Debug.java
+++ src/Debug.java
@@ -1,8 +1,14 @@
 public class Debug {
     public static void main(String[] args) throws Exception{
-      long start = System.currentTimeMillis();
-      Main.main(new String[0]);
-      long end = System.currentTimeMillis();
-      System.out.println((end - start)  + "ms");
+        doMain();
     }
+    
+    
+    public static void doMain() throws Exception{
+        long start = System.currentTimeMillis();
+        Main.main(new String[0]);
+        long end = System.currentTimeMillis();
+        System.out.println((end - start)  + "ms");
+    }
+    
 }

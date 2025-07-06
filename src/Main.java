c1385f00b02ffd5a4c9c41bb3b5c41595b0b6fe3
checker888 <ort030627@gmail.com>
2025-07-07 02:34:07 +0900

    Mainとメモ書き分離

--- src/Main.java
+++ src/Main.java
@@ -15,24 +15,7 @@ import java.util.PriorityQueue;
 import java.util.Scanner;
 //import java.io.File;
 
-//    int n = Integer.parseInt(sc.next());
-//    int [] a = arrayInputInt(n);
-//    int [][] a = arrayInputInt(y, x);
-//    String s = sc.next();
-//    String w [] = s.split("");
-//    List<Integer> list = new ArrayList<Integer>();
-//    HashMap<String,Integer> map = new HashMap<String,Integer>();
 
-//    Deque<Integer> dq = new ArrayDeque<>();
-//    TreeSet<Integer> set = new TreeSet<Integer>();
-//    PriorityQueue<Integer> pq = new PriorityQueue<Integer>(Collections.reverseOrder());
-
-//    for(String key : map.keySet()){}
-//    Arrays.sort(aw,Collections.reverseOrder());
-//    System.out.println(String.format("%.1f", 21.8755));
-//    int bitCount = Integer.bitCount(num);
-//    Arrays.sort(bx, (a, b) -> Long.compare(Math.abs(a), Math.abs(b)));
-//ーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー
 public class Main {
     static Scanner sc;
     static PrintWriter pw = new PrintWriter(System.out);
@@ -1063,30 +1046,3 @@ class Four<S extends Comparable<S>, T extends Comparable<T>, U extends Comparabl
     }
 }
 
-
-
-
-
-//配列の最大・最小値取得
-//Arrays.stream([配列名]).min().getAsInt()
-//Arrays.stream([配列名]).max().getAsInt()
-
-
-
-//grid 上下左右・斜めの接続
-//for(int i=0;i<h;i++) {
-//    for(int j=0;j<w;j++) {
-//        int num = (i*w)+j;
-//        if(grid[i][j].equals("#")) {
-//            if(i != 0 && grid[i-1][j].equals("#")) g.connect(num, num-w);
-//            if(i != h-1 && grid[i+1][j].equals("#"))g.connect(num, num+w);
-//            if(j != 0 && grid[i][j-1].equals("#"))g.connect(num, num-1);
-//            if(j != w-1 && grid[i][j+1].equals("#"))g.connect(num, num+1);
-//        
-//            if(i != 0 && j != 0 && grid[i-1][j-1].equals("#")) g.connect(num, num-w-1);
-//            if(i != 0 && j != w-1 && grid[i-1][j+1].equals("#")) g.connect(num, num-w+1);
-//            if(i != h-1 && j != 0 && grid[i+1][j-1].equals("#")) g.connect(num, num+w-1);
-//            if(i != h-1 && j != w-1 && grid[i+1][j+1].equals("#")) g.connect(num, num+w+1);
-//         }
-//    }
-//}

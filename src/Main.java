53011480f78538fdd7cabc7ce51f161f36b93de6
checker888 <ort030627@gmail.com>
2025-07-29 00:48:26 +0900

    Main:eps追加

--- src/Main.java
+++ src/Main.java
@@ -23,6 +23,7 @@ public class Main {
     static int mod = 1000000007;
     static int bigint = 2000000000;
     static long biglong = 2000000000000000000L;
+    static double eps = 1e-6;
     public static void main(String[] args) throws Exception{//ーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー
         sc = new Scanner(       System.in       );
 //      sc = new Scanner(       new File("src/testcase.txt")         );
